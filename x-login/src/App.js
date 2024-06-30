564b7cf6773d26cf8e0617c5bae56657fd29aa17
shrwn08 <shrwnkumar08@gmail.com>
2024-06-30 13:06:38 +0530

    completed

---
+++ x-login/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [getUsername, setGetUsername] = useState("");
+  const [getPassword, setGetPassword] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmitBtn = (e) => {
+    e.preventDefault();
+    if (getUsername === "user" && getPassword === "password") {
+      setSubmit(true);
+    } else {
+      setSubmit(false);
+    }
+  };
+  const handleOnChangeUsername = (e) => {
+    setGetUsername(e.target.value);
+  };
+  const handleOnChangePassword = (e) => {
+    setGetPassword(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Login Page</h1>
+      {!submit ? (
+        <form onSubmit={handleSubmitBtn}>
+          <label>Username: </label>
+          <input
+            type="text"
+            value={getUsername}
+            onChange={handleOnChangeUsername}
+            placeholder="username"
+            required
+          />
+          <br />
+          <label>Password: </label>
+          <input
+            type="password"
+            value={getPassword}
+            onChange={handleOnChangePassword}
+            placeholder="password"
+            required
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <p>Welcome, user!</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
